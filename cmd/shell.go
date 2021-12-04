7cf51d90fdea1921317805bfd968d0cb766aa377
FlorianWoelki <FlorianWoelki@gmx.de>
2021-12-04 11:03:40 +0100

    feat: add first repl

--- cmd/shell.go
+++ cmd/shell.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"os/user"
 
-	"github.com/florianwoelki/reflow/lexer"
+	"github.com/florianwoelki/reflow/repl"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("reflow > ")
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
-
-		l := lexer.New(text)
-		for i := 0; i < len(text); i++ {
-			token := l.NextToken()
-			fmt.Printf("[type: %s, literal: %s] ", token.Type, token.Literal)
-		}
-		fmt.Println()
-
-		if strings.Compare("exit", text) == 0 {
-			os.Exit(1)
-		}
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Printf("Hello %s! This is the reflow programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Run(os.Stdin, os.Stdout)
 }
