394fc8aec8834fb002932386fd8d40307b972ee6
FlorianWoelki <FlorianWoelki@gmx.de>
2021-12-14 12:47:54 +0100

    feat(parser): add infix parsing

--- ast/ast.go
+++ ast/ast.go
@@ -40,6 +40,31 @@ type Program struct {
 	Statements []Statement
 }
 
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
