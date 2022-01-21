ce6190c115db63315317453e10d9b4ccea3d6c0b
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-21 10:33:39 +0100

    feat(repl): add evaluation functionality

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 
+	"github.com/florianwoelki/reflow/evaluator"
 	"github.com/florianwoelki/reflow/lexer"
 	"github.com/florianwoelki/reflow/parser"
 )
@@ -30,8 +31,11 @@ func Run(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
