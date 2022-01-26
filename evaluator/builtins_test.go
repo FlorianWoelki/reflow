b4e8b57a6382e06959f9c7a0d13264b449c30fac
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-26 12:09:02 +0100

    test(eval): add check for unequal length

--- evaluator/builtins_test.go
+++ evaluator/builtins_test.go
@@ -7,6 +7,10 @@ import (
 func TestBuiltins(t *testing.T) {
 	expectedFunctions := []string{"len", "print", "str", "first", "last", "rest", "pop", "push", "map", "find", "filter"}
 
+	if len(builtins) != len(expectedFunctions) {
+		t.Errorf("length of expected functions is not the same as builtins. expected=%d, got=%d", len(builtins), len(expectedFunctions))
+	}
+
 	for _, function := range expectedFunctions {
 		_, ok := builtins[function]
 		if !ok {
