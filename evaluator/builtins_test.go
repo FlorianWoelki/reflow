4787567e243fce2ea3457ca1a6833c3b4e70b788
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-27 08:59:26 +0100

    test(eval): add tests for built in len function

--- evaluator/builtins_test.go
+++ evaluator/builtins_test.go
@@ -1,7 +1,10 @@
 package evaluator
 
 import (
+	"strconv"
 	"testing"
+
+	"github.com/florianwoelki/reflow/object"
 )
 
 func TestBuiltins(t *testing.T) {
@@ -18,4 +21,42 @@ func TestBuiltins(t *testing.T) {
 			continue
 		}
 	}
+}
+
+func TestBuiltinLen(t *testing.T) {
+	result := builtinLen()
+	if result.Type() != object.ERROR_OBJ {
+		t.Errorf("result for empty args is of wrong type. expected=%s, got=%s", object.ERROR_OBJ, result.Type())
+	}
+
+	str := "Hello World"
+	result = builtinLen(&object.String{Value: str})
+	if result.Type() != object.INTEGER_OBJ {
+		t.Errorf("result for string is of wrong type. expected=%s, got=%s", object.INTEGER_OBJ, result.Type())
+	}
+
+	parsedLen, err := strconv.Atoi(result.Inspect())
+	if err != nil {
+		t.Errorf("received error while parsing length of string. got=%s", err)
+	}
+
+	if parsedLen != len(str) {
+		t.Errorf("result for string is of wrong length. expected=%d, got=%d", len(str), parsedLen)
+	}
+
+	arr := &object.Array{Elements: []object.Object{&object.Integer{Value: 1}, &object.Integer{Value: 1}}}
+	result = builtinLen(arr)
+	if result.Type() != object.INTEGER_OBJ {
+		t.Errorf("result for array is of wrong type. expected=%s, got=%s", object.INTEGER_OBJ, result.Type())
+	}
+
+	parsedLen, err = strconv.Atoi(result.Inspect())
+	if err != nil {
+		t.Errorf("received error while parsing length of array. got=%s", err)
+	}
+
+	if parsedLen != len(arr.Elements) {
+		t.Errorf("result for array is of wrong length. expected=%d, got=%d", len(str), parsedLen)
+	}
+
 }
