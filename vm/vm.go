bb2b53b10a3901287069ed306991a5894ba10e49
FlorianWoelki <FlorianWoelki@gmx.de>
2022-02-18 10:05:59 +0100

    feat(vm): add pop opcode handling

--- vm/vm.go
+++ vm/vm.go
@@ -47,10 +47,16 @@ func (vm *VM) Run() error {
 			rightValue := right.(*object.Integer).Value
 			result := leftValue + rightValue
 			vm.push(&object.Integer{Value: result})
+		case code.OpPop:
+			vm.pop()
 		}
 	}
 
 	return nil
+}
+
+func (vm VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
 }
 
 func (vm *VM) pop() object.Object {
